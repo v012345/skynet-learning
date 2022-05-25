6016aa7baf2e599c1fa174fe045f00f4a7f1f46c
v138468 <v138468@gmail.com>
2022-05-25 09:38:08 +0800

    tst

--- src/main.lua
+++ src/main.lua
@@ -6,12 +6,18 @@ skynet.start(function()
     -- 启动打工服务，其中第二个参数和第三个参数会透传给service/worker/init.lua脚本
     local worker1 = skynet.newservice("worker", "worker", 1)
 
+    -- 启动买猫粮服务
+    local buy1 = skynet.newservice("buy", "buy", 1)
+
     -- 开始工作
     skynet.send(worker1, "lua", "start_work")
     -- 主服务休息2秒，注意，这里是主服务休息2秒，并不会卡住worker服务
     skynet.sleep(200)
     -- 停止工作
     skynet.send(worker1, "lua", "stop_work")
+
+    -- 买猫粮
+    skynet.send(buy1, "lua", "buy")
 
 
     skynet.exit()
